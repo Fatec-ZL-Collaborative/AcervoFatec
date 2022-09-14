a878b60c3b3fdec2fb13ee736218a0d5412b86fe
mateussantanasilva <santanasilva1778@gmail.com>
2022-09-14 13:03:12 -0300

    :tada: base do projeto

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// =============== INITIAL SETTINGS ===============
+window.addEventListener('scroll', onScroll)
+const bodyElement = document.body
+
+function onScroll() {
+    const menu = document.querySelector('#header')
+    const AlreadyHaveClass = menu.classList.contains('fixed-menu')
+
+    if((scrollY >= 0) && (!AlreadyHaveClass)) {
+        changeColorMenuOnScroll(menu)
+    }
+
+    if((scrollY <= 0) && (AlreadyHaveClass)){
+        menu.classList.remove('fixed-menu')
+    }
+}
+
+// =============== MENU ===============
+function changeColorMenuOnScroll(menu) {
+    menu.classList.add('fixed-menu')
+}
+
+// =============== SCROLL REVEAL SETTINGS ===============
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: '800',
+    delay: 150
+})
+
+scrollReveal.reveal(`
+    .main-container_presentation,
+    .footer-container
+`)
+
+// =============== DATATABLES SETTINGS ===============
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
