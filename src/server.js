574be2723d0500ddd3a124d02b795f9252eb019e
mateussantanasilva <santanasilva1778@gmail.com>
2022-10-17 14:23:59 -0300

    :lipstick: definição da estrutura da barra de acessibilidade

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const connection = require('./model/services/db')
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'))
 
 app.use(express.json())
 
-app.get('/', connection)
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000, console.log(`Example app listening on port 3000! - http://localhost:3000/`))
